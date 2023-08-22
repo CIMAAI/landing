b5d0bbb9b756dc1f0973a23861c36db90d1cc7a5
Kevin Colten <kevincolten@gmail.com>
2023-08-22 13:01:57 -0700

    tina

---
+++ tina/config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "e1bb6772-79a4-4fb9-a598-cbe2f03a8555", // Get this from tina.io
+  token: "633dc539d735a69b9dde8b6242335f1fc252dbc7", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
