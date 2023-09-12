f3311e13b2ec2ff7b99879fb978da3d4a1103d4c
Kevin Colten <kevincolten@gmail.com>
2023-09-12 07:38:09 -0700

    scripts

---
+++ _scripts/configs.js
@@ -0,0 +1,8 @@
+const fm = require('front-matter');
+const { readFileSync, writeFileSync, existsSync, mkdirSync } = require('fs');
+
+(() => {
+  const clientKeys = fm(readFileSync('./settings.md', 'utf-8')).attributes.clients.map(client => client.client_key).sort();
+  if (!existsSync('./_configs')) mkdirSync('./_configs');
+  clientKeys.forEach(clientKey => writeFileSync(`./_configs/${clientKey}.yml`, `client_key: ${clientKey}`));
+})();
--- _scripts/deploy.js
+++ _scripts/deploy.js
@@ -1,14 +1,12 @@
 const fm = require('front-matter');
-const { readFileSync, writeFileSync, existsSync, mkdirSync } = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
 const { execSync } = require('child_process');
 
 (() => {
   const clientKeys = fm(readFileSync('./settings.md', 'utf-8')).attributes.clients
     .map(client => client.client_key).sort();
   console.log(clientKeys)
-  if (!existsSync('./_configs')) mkdirSync('./_configs');
   clientKeys.forEach(clientKey => {
-    writeFileSync(`./_configs/${clientKey}.yml`, `client_key: ${clientKey}`);
     const clientKeyParts = clientKey.split('_');
     const command = `jekyll build --config _config.yml,_configs/${clientKey}.yml --destination _site/${clientKeyParts[0]}/${clientKeyParts[1] || ''}`;
     console.log(command);
